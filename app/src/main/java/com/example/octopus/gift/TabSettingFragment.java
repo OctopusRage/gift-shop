aced23fba9c4b9aa9cf6d8b00da249784534bb31
octopus <ahyana1995@gmail.com>
2017-12-20 22:36:27 +0700

    add item and icon and swipe navigation

---
+++ app/src/main/java/com/example/octopus/gift/TabSettingFragment.java
@@ -0,0 +1,20 @@
+package com.example.octopus.gift;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by octopus on 12/20/17.
+ */
+
+public class TabSettingFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_setting_fragment, container, false);
+    }
+}
